83e5a019922247e51085a0e15a7900285c53e68d
sonnv1801 <nguyenvanson18102001@gmail.com>
2022-07-06 08:41:30 +0700

    update code

--- ShopAccessory/src/java/dbcontext/DBUtil.java
+++ ShopAccessory/src/java/dbcontext/DBUtil.java
@@ -5,10 +5,43 @@
  */
 package dbcontext;
 
+import static dbcontext.DatabaseInfor.driverName;
+import static dbcontext.DatabaseInfor.pass;
+import static dbcontext.DatabaseInfor.url;
+import static dbcontext.DatabaseInfor.user;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author PC
  */
-public class DBUtil {
-    
+public class DBUtil extends DatabaseInfor {
+
+    private static DBUtil instance;
+
+    public DBUtil() {
+    }
+
+    public Connection getConnection() throws Exception {
+        Class.forName(driverName);
+        Connection con = DriverManager.getConnection(url, user, pass);
+        return con;
+
+    }
+
+    //Get instance of dbms only one time
+    public static DBUtil getInstance() {
+        if (instance == null) {
+            instance = new DBUtil();
+        }
+        return instance;
+    }
+
+    public static void closeConnection(Connection connection) throws SQLException {
+        if (connection != null) {
+            connection.close();
+        }
+    }
 }
--- ShopAccessory/src/java/dbcontext/DatabaseInfor.java
+++ ShopAccessory/src/java/dbcontext/DatabaseInfor.java
@@ -10,5 +10,8 @@ package dbcontext;
  * @author PC
  */
 public class DatabaseInfor {
-    
+     public static String driverName="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    public static String url="jdbc:sqlserver://127.0.0.1:1433;databaseName=LoginShoppingCart;";
+    public static String user="sa";
+    public static String pass="vanson123";
 }
